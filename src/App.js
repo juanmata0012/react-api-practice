c902843ffe6d471bef8aea8b7d92553f455a7a6e
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2017-06-13 20:18:36 -0500

    called api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      apiResult: ""
+    }
+  }
+  componentWillMount() {
+    // Make a request for a user with a given ID
+axios.get('https://api.nasa.gov/planetary/apod?api_key=gWqPuLvkWkZRIaWFw76qxLLTsNlsJeYycY6wY2Ec')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  
+
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +33,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div> 
+      </div>
     );
   }
 }
