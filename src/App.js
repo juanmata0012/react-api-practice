5f158f909a0152a80ddbe0d0d2f19fc1080a2600
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2017-06-13 20:53:46 -0500

    api title and url

--- src/App.js
+++ src/App.js
@@ -7,28 +7,30 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      apiResult: ""
+      apiResult: "",
+      apiTitle: ""
     }
   }
   componentWillMount() {
     // Make a request for a user with a given ID
 axios.get('https://api.nasa.gov/planetary/apod?api_key=gWqPuLvkWkZRIaWFw76qxLLTsNlsJeYycY6wY2Ec')
-  .then(function (response) {
+  .then((response) => {
     console.log(response);
+    this.setState({apiResult: response.data.hdurl})
+    this.setState({apiTitle: response.data.title})
   })
-  .catch(function (error) {
+  .catch((error) => {
     console.log(error);
   });
 
-  
 
   }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={this.state.apiResult} className="App-logo" alt="logo" />
+          <h2>{this.state.apiTitle}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
